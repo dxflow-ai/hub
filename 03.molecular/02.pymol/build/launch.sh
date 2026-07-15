794eb0a62bf76fd72f5d93bebdc13db0bc5df6c9
Amir Reza Dalir <dalirnet@gmail.com>
2026-07-15 21:31:53 +0330

    molecular: wait for and maximize the pymol window on launch

--- 03.molecular/02.pymol/build/launch.sh
+++ 03.molecular/02.pymol/build/launch.sh
@@ -5,3 +5,11 @@ echo "Launching ..."
 
 # Start the application
 nohup pymol &
+
+# Wait for the application to start
+while ! wmctrl -l | grep "PyMOL"; do
+  sleep 1
+done
+
+# Maximize the application window
+wmctrl -r "PyMOL" -b add,maximized_vert,maximized_horz
